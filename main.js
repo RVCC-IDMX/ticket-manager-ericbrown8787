85c9e43d7c16fe9b3ff37f8e43e379166800090f
Eric Brown <eric.brown8787@gmail.com>
2022-10-20 12:24:24 -0400

    Create firstEventEmitter
    Emits a single event as a test

--- main.js
+++
@@ -1,6 +0,0 @@
-import chalk from 'chalk';
-
-const { log } = console;
-
-log(chalk.red('This is some red text right here'));
-log(chalk.magenta('This sure is some magenta text right here'));
---
+++ ticketManager.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+const { log } = console;
+
+// log(chalk.red('This is some red text right here'));
+// log(chalk.magenta('This sure is some magenta text right here'));
