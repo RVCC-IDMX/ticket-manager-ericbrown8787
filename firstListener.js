84ade2eea26c52cde3f4517819841df9d569d624
Eric Brown <eric.brown8787@gmail.com>
2022-10-20 14:46:22 -0400

    Add once listener
    Add and test listener using once

--- firstListener.js
+++ firstListener.js
@@ -5,3 +5,12 @@ const { log } = console;
 const ticketManager = new TicketManager(10);
 
 ticketManager.on('buy', () => { log(chalk.green('Someone bought a ticket!')); });
+ticketManager.once('buy', () => {
+  log(chalk.cyanBright('This is only called once'));
+});
+ticketManager.buy('test@email.com', 20);
+ticketManager.buy('test@email.com', 20);
+
+ticketManager.once('buy', () => {
+  log(chalk.cyanBright('This is only called once'));
+});
