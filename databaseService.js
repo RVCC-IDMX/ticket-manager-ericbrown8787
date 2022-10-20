2a757ce633542fa6a0f4ef621e244c0160c6b1a2
Eric Brown <eric.brown8787@gmail.com>
2022-10-20 16:20:18 -0400

    Create databaseService and emailService

---
+++ databaseService.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+
+const { log } = console;
+
+class DatabaseService {
+  static save(email, price, timestamp) {
+    log(chalk.blue(`Running query: INSERT INTO orders VALUES
+    (email, price, created) VALUES (${email}, ${price}, ${timestamp})`));
+  }
+}
---
+++ emailService.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+
+const { log } = console;
+
+class EmailService {
+  static send(email) {
+    log(chalk.magenta(`Sending email to ${email}`));
+  }
+}
