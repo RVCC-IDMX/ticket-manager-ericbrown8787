5707bcf897a7e8e0f8465f35e11c9da65152f591
Eric Brown <eric.brown8787@gmail.com>
2022-10-20 01:19:23 -0400

    Install Chalk
    Install package, import and test

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('This works! ^_^');
+import chalk from 'chalk';
+
+const { log } = console;
+
+log(chalk.red('This is some red text right here'));
+log(chalk.magenta('This sure is some magenta text right here'));
